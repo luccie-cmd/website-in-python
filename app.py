34dd3d3efe19056bb04b3ca1a702a7d9770e2509
luccie-cmd <hoedje201@gmail.com>
2023-03-05 11:34:52 +0100

    added password and email files

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template,  request
 
 app = Flask(__name__)
-emails = ["test@gmail.com"]
-passwords = ["test1234"]
+with open("emails.txt") as email_file:
+    emails = email_file.read().split()
+with open("passwords.txt") as passwords_file:
+    passwords = passwords_file.read().split()
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,7 +16,10 @@ def index():
     if request.method == 'POST':
         if request.form.get('sign_in') == 'sign in':
             return redirect(url_for('sign_in'))
+        elif request.form.get('sign_up_page') == 'sign up':
+            return redirect(url_for('sign_up'))
         elif request.form.get('login') == 'login':
+            global email, password
             email = request.form.get('email')
             password = request.form.get('password')
             for i in emails:
@@ -22,13 +27,33 @@ def index():
                     if email == i and password == j:
                         return redirect(url_for('login'))
             return redirect(url_for('sign_in'))
+        elif request.form.get('sign_up_add') == 'sign up':
+            email = request.form.get('email')
+            password = request.form.get('password')
+            if email not in emails and password not in passwords:
+                with open('passwords.txt', "r") as password_file:
+                    original = password_file.read()
+                with open('passwords.txt', "w") as password_file:
+                    password_file.write(original + " " + password)
+                with open('emails.txt', "r") as email_file:
+                    original = email_file.read()
+                with open('emails.txt', "w") as email_file:
+                    email_file.write(original + " " + email)
+                return redirect(url_for('login'))
+            else:
+                return redirect(url_for('sign_in'), 302)
         else:
             return redirect(url_for('False_Button'))
     return render_template("index.html")
 
 @app.route('/sign_in')
 def sign_in():
+    print(emails, passwords)
     return render_template("sign_in.html")
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template("sign_up.html")
 
 @app.route('/False_Button')
 def False_Button():
@@ -39,4 +64,4 @@ def login():
     return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(None, 5500, True)
+    app.run("Lucas Bos", 5500, True)
